15b743f55003c05b3b7522e0adca2f111067d273
Kuda_Valen <kudamukwasi@gmail.com>
2026-08-11 15:38:47 +0200

    learning binary search

---
+++ searching.py
@@ -0,0 +1,22 @@
+numbers = [3, 5, 8, 9, 13, 16, 17, 19, 25, 36, 47, 49, 50]
+
+def search_number(num):
+    a = []               # this will represent the small list
+    l = len(numbers)
+    i = 0
+    found = False
+    half = l // 2
+
+    if num >= numbers[half] and num in numbers:
+        print("Search last half")
+    elif num <= numbers[half] and num in numbers:
+        print("Seach first half")
+    else:
+        print("Number not found!!")
+
+if __name__ == "__main__":
+    print("\n=== Binary Search Algorithm ===\n")
+    print(numbers)
+    user_input = int(input("\nEnter nubmer to search for: "))
+    search_number(user_input)
+    
